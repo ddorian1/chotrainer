527091236e15ee6e74950bd4565e6511fd4d157a
Johannes Schwab <mail@jschwab.org>
2017-02-22 00:27:07 +0100

    translation

--- intl/chotrainer_de.ts
+++ intl/chotrainer_de.ts
@@ -4,73 +4,82 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../chotrain/MainWindow.cpp" line="26"/>
+        <location filename="../chotrainer/MainWindow.cpp" line="37"/>
         <source>From bar</source>
         <translation>Ab Takt</translation>
     </message>
     <message>
-        <location filename="../chotrain/MainWindow.cpp" line="50"/>
+        <location filename="../chotrainer/MainWindow.cpp" line="63"/>
         <source>Only my voice</source>
         <translation>Nur meine Stimme</translation>
     </message>
     <message>
-        <location filename="../chotrain/MainWindow.cpp" line="51"/>
+        <location filename="../chotrainer/MainWindow.cpp" line="64"/>
         <source>Emphatize my voice</source>
         <translation>Meine Stimme hervorheben</translation>
     </message>
     <message>
-        <location filename="../chotrain/MainWindow.cpp" line="52"/>
+        <location filename="../chotrainer/MainWindow.cpp" line="65"/>
         <source>Don&apos;t emphatize my voice</source>
         <translation>Meine Stimme nicht hervorheben</translation>
     </message>
     <message>
-        <location filename="../chotrain/MainWindow.cpp" line="53"/>
+        <location filename="../chotrainer/MainWindow.cpp" line="66"/>
         <source>Mute my voice</source>
         <translation>Meine Stimme stummschalten</translation>
     </message>
     <message>
-        <location filename="../chotrain/MainWindow.cpp" line="54"/>
-        <location filename="../chotrain/MainWindow.cpp" line="84"/>
+        <location filename="../chotrainer/MainWindow.cpp" line="67"/>
+        <location filename="../chotrainer/MainWindow.cpp" line="98"/>
         <source>Play</source>
         <translation>Play</translation>
     </message>
     <message>
-        <location filename="../chotrain/MainWindow.cpp" line="89"/>
+        <location filename="../chotrainer/MainWindow.cpp" line="108"/>
         <source>Stop</source>
         <translation>Stop</translation>
     </message>
     <message>
-        <location filename="../chotrainCreator/MainWindow.cpp" line="52"/>
+        <location filename="../chotrainerCreator/MainWindow.cpp" line="74"/>
         <source>The name of this voice</source>
         <translation>Name dieser Stimme</translation>
     </message>
     <message>
-        <location filename="../chotrainCreator/MainWindow.cpp" line="58"/>
+        <location filename="../chotrainerCreator/MainWindow.cpp" line="80"/>
         <source>Check if this voice is part of the accompaniment</source>
         <translation>Auswählen, wenn diese Stimme Teil der Begleitung ist</translation>
     </message>
     <message>
-        <location filename="../chotrainCreator/MainWindow.cpp" line="63"/>
         <source>Name</source>
-        <translation>Name</translation>
+        <translation type="vanished">Name</translation>
     </message>
     <message>
-        <location filename="../chotrainCreator/MainWindow.cpp" line="64"/>
+        <location filename="../chotrainerCreator/MainWindow.cpp" line="62"/>
+        <source>Name of piece</source>
+        <translation>Name des Stückes</translation>
+    </message>
+    <message>
+        <location filename="../chotrainerCreator/MainWindow.cpp" line="85"/>
+        <source>Name of voice</source>
+        <translation>Name der Stimme</translation>
+    </message>
+    <message>
+        <location filename="../chotrainerCreator/MainWindow.cpp" line="86"/>
         <source>Accompaniment</source>
         <translation>Begleitung</translation>
     </message>
     <message>
-        <location filename="../chotrainCreator/MainWindow.cpp" line="69"/>
+        <location filename="../chotrainerCreator/MainWindow.cpp" line="91"/>
         <source>Play this voice</source>
         <translation>Diese Stimme abspielen</translation>
     </message>
     <message>
-        <location filename="../chotrainCreator/MainWindow.cpp" line="84"/>
+        <location filename="../chotrainerCreator/MainWindow.cpp" line="107"/>
         <source>Close</source>
         <translation>Beenden</translation>
     </message>
     <message>
-        <location filename="../chotrainCreator/MainWindow.cpp" line="85"/>
+        <location filename="../chotrainerCreator/MainWindow.cpp" line="108"/>
         <source>Save</source>
         <translation>Speichern</translation>
     </message>
@@ -78,26 +87,80 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../chotrain/Main.cpp" line="33"/>
-        <location filename="../chotrainCreator/Main.cpp" line="41"/>
+        <location filename="../chotrainer/Main.cpp" line="30"/>
+        <location filename="../chotrainerCreator/Main.cpp" line="29"/>
         <source>Open File</source>
         <translation>Datei öffnen</translation>
     </message>
     <message>
-        <location filename="../chotrain/Main.cpp" line="33"/>
-        <location filename="../chotrainCreator/MainWindow.cpp" line="100"/>
-        <source>Chotrain file</source>
-        <translation>Chotrain-Datei</translation>
+        <location filename="../chotrainer/Main.cpp" line="30"/>
+        <location filename="../chotrainerCreator/MainWindow.cpp" line="124"/>
+        <source>Chotrainer file</source>
+        <translation>Chotrainer-Datei</translation>
     </message>
     <message>
-        <location filename="../chotrainCreator/Main.cpp" line="41"/>
+        <location filename="../chotrainer/Main.cpp" line="43"/>
+        <location filename="../chotrainer/MainWindow.cpp" line="104"/>
+        <location filename="../chotrainerCreator/Main.cpp" line="40"/>
+        <location filename="../chotrainerCreator/MainWindow.cpp" line="168"/>
+        <source>Critical Error</source>
+        <translation>Kritische Fehler</translation>
+    </message>
+    <message>
+        <location filename="../chotrainer/Main.cpp" line="43"/>
+        <source>Can&apos;t process Chotrainer file:
+%1</source>
+        <translation>Kann Chotrainer-Datei nicht verarbeiten:
+%1</translation>
+    </message>
+    <message>
+        <source>Chotrain file</source>
+        <translation type="vanished">Chotrain-Datei</translation>
+    </message>
+    <message>
+        <location filename="../chotrainerCreator/Main.cpp" line="29"/>
         <source>Midi file</source>
         <translation>Midi-Datei</translation>
     </message>
     <message>
-        <location filename="../chotrainCreator/MainWindow.cpp" line="100"/>
+        <location filename="../chotrainerCreator/Main.cpp" line="40"/>
+        <source>Can&apos;t process midi file:
+%1</source>
+        <translation>Kann Midi-Datei nicht verarbeiten:
+%1</translation>
+    </message>
+    <message>
+        <location filename="../chotrainerCreator/MainWindow.cpp" line="124"/>
         <source>Save File</source>
         <translation>Datei speichern</translation>
+    </message>
+    <message>
+        <location filename="../chotrainerCreator/MainWindow.cpp" line="143"/>
+        <source>Warning</source>
+        <translation>Warnung</translation>
+    </message>
+    <message>
+        <location filename="../chotrainerCreator/MainWindow.cpp" line="143"/>
+        <source>Can&apos;t save to file:
+%1</source>
+        <translation>Speichern in Datei nicht möglich:
+%1</translation>
+    </message>
+    <message>
+        <location filename="../chotrainer/MainWindow.cpp" line="104"/>
+        <location filename="../chotrainerCreator/MainWindow.cpp" line="168"/>
+        <source>Can&apos;t create midi file:
+%1</source>
+        <translation>Kann Midi-Datei nicht erstellen:
+%1</translation>
+    </message>
+</context>
+<context>
+    <name>SelectVoiceDialog</name>
+    <message>
+        <location filename="../chotrainer/SelectVoiceDialog.cpp" line="25"/>
+        <source>Please select your voice</source>
+        <translation>Bitte Ihrer Stimme auswählen</translation>
     </message>
 </context>
 </TS>
